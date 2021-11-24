ef301619783f5a983dcfc03647b90c5c77ca7a0c
Ju Monteiro <julianacmonteiro@hotmail.com>
2021-11-24 11:40:43 -0300

    alteracoes de precadastro

---
+++ cypress/integration/pre-cadastro.spec.js
@@ -0,0 +1,25 @@
+/// <reference types = "cypress" />
+var faker = require('faker');
+
+describe('funcionalidade pré cadastro', () => {
+
+
+    beforeEach(() => {
+        cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+
+    it('deve completar o pré cadastro com sucesso', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type('123teste@123$')
+        cy.get(':nth-child(4) > .button').click()
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.name.firstName())
+        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('.woocommerce-Button').click()
+
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+    });
+    
+});
