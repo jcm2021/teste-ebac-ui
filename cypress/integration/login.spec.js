d7b1074ef72474bf1e49d71a8c598230bc27d43a
Ju Monteiro <julianacmonteiro@hotmail.com>
2021-11-25 11:50:22 -0300

    usando massa de dados

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,39 +1,63 @@
 /// <reference types = "cypress" />
+const perfil = require('../fixtures/perfil.json')
 
 context('funcionalidade login', () => {
 
     beforeEach(() => {
-        cy.visit ("http://lojaebac.ebaconline.art.br/minha-conta/")
+        cy.visit("minha-conta")
 
-       
+
     });
 
     afterEach(() => {
         cy.screenshot()
     });
 
-    it ('Deve fazer login com sucesso', () => {
+    it('deve fazer login com sucesso', () => {
 
-cy.get('#username').type ("aluno_ebac@teste.com")
-cy.get('#password').type ("teste@teste.com")
-cy.get('.woocommerce-form > .button') .click ()
+        cy.get('#username').type("aluno_ebac@teste.com")
+        cy.get('#password').type("teste@teste.com")
+        cy.get('.woocommerce-form > .button').click()
 
-cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should ('contain', "Olá")
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', "Olá")
     })
 
-    it ('deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
-        
-cy.get('#username').type ("aluno_ebac20@teste.com")
-cy.get('#password').type ("teste@teste.com")
-cy.get('.woocommerce-form > .button') .click ()
+    it('deve fazer login com sucesso - usando arquivo de dados', () => {
 
-})
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
 
-it ('deve exibir uma mensagem de erro ao inserir a senha inválida', () => {
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', "Olá")
 
-    cy.get('#username').type ("aluno_ebac@teste.com")
-    cy.get('#password').type ("teste@123teste.com")
-    cy.get('.woocommerce-form > .button') .click () 
-})
-   
+    });
+
+    it.only('deve fazer login com sucesso - usando fixture', () => {
+
+        cy.fixture('perfil').then(dados => {
+
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha, {log: false})
+            cy.get('.woocommerce-form > .button').click()
+
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', "Olá")
+        })
+
+    });
+
+    it('deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+
+        cy.get('#username').type("aluno_ebac20@teste.com")
+        cy.get('#password').type("teste@teste.com")
+        cy.get('.woocommerce-form > .button').click()
+
+    })
+
+    it('deve exibir uma mensagem de erro ao inserir a senha inválida', () => {
+
+        cy.get('#username').type("aluno_ebac@teste.com")
+        cy.get('#password').type("teste@123teste.com")
+        cy.get('.woocommerce-form > .button').click()
+    })
+
 })
--- cypress/integration/produtos.spec.js
+++ cypress/integration/produtos.spec.js
@@ -3,7 +3,7 @@
 describe('funcionalidade página de produtos', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+        cy.visit('produtos')
     });
 
     it('deve selecionar um produto da lista', () => {
